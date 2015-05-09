ad1a063c6f525497b0b4e980773c7727849a29da
Haellsigh <Haellsigh@gmail.com>
2015-05-09 02:22:36 +0200

    Added 'about' state

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,19 +1,23 @@
 var hack = angular.module('hack');
 
 hack.config(['$stateProvider', '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider
-		.otherwise("/");
-	
-	$stateProvider
-		.state('home', {
+    function ($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider
+			.otherwise("/");
+
+		$stateProvider
+			.state('home', {
 			url: '/',
-	        templateUrl: 'app/components/home/view.html',
-	        controller: 'homeController'
-      	})
-		.state('upload', {
+			templateUrl: 'app/components/home/view.html',
+			controller: 'homeController'
+		})
+			.state('about', {
+			url: '/about',
+			templateUrl: 'app/components/about/view.html'
+		})
+			.state('upload', {
 			url: '/upload',
 			templateUrl: 'app/shared/upload/view.html',
 			controller: 'uploadController'
 		});
-}]);
+	}]);
